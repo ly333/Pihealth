5765ef8bfa0af6a368eb3e0d344302ea65df0c31
ly333 <596890983@qq.com>
2018-06-14 21:16:16 +0800

    头文件

---
+++ head.h
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: head.h
+	> Author: luoyi
+	> Mail: 596890983@qq.com
+	> Created Time: 2018年03月06日 星期二 16时49分19秒
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
+#endif
